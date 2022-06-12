2810945ca4b0f0b3c4e5f18c84fd3a1fb5dab103
BenjaminLoggenberg <bloggenberg97@gmail.com>
2022-06-12 18:17:00 +0200

    add text
    add color text

--- rainbowChallenge/myExternalFile.js
+++ rainbowChallenge/myExternalFile.js
@@ -1,28 +1,35 @@
 function changeRed() {
     const redDiv = document.getElementById("red");
     redDiv.style.backgroundColor = "red";
+    document.getElementById("red").innerHTML = "RED";
 }
 function changeOrange() {
     const redDiv = document.getElementById("orange");
     redDiv.style.backgroundColor = "orange";
+    document.getElementById("orange").innerHTML = "ORANGE";
 }
 function changeYellow() {
     const redDiv = document.getElementById("yellow");
     redDiv.style.backgroundColor = "yellow";
+    document.getElementById("yellow").innerHTML = "YELLOW";
 }
 function changeGreen() {
     const redDiv = document.getElementById("green");
     redDiv.style.backgroundColor = "green";
+    document.getElementById("green").innerHTML = "GREEN";
 }
 function changeBlue() {
     const redDiv = document.getElementById("blue");
     redDiv.style.backgroundColor = "blue";
+    document.getElementById("blue").innerHTML = "BLUE";
 }
 function changePurple() {
     const redDiv = document.getElementById("purple");
     redDiv.style.backgroundColor = "purple";
+    document.getElementById("purple").innerHTML = "PURPLE";
 }
 function changePink() {
     const redDiv = document.getElementById("pink");
     redDiv.style.backgroundColor = "pink";
+    document.getElementById("pink").innerHTML = "PINK";
 }

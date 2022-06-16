264e7233685a325477cc2ce0bf03ecbde5e7192e
BenjaminLoggenberg <bloggenberg97@gmail.com>
2022-06-16 10:20:51 +0200

    Three Input Calculator Challenge

--- oddEvenChallenge/myExternalFile.js
+++ oddEvenChallenge/myExternalFile.js
@@ -1,10 +1,29 @@
 let userInput = prompt("Please input a number:");
 
+console.log(typeof userInput);
+
 let numberConv = parseInt(userInput);
+
+console.log(typeof numberConv);
 
 function oddEven() {
 
-    if (numberConv.!= Number) 
+    if (typeof numberConv !== "number") {
+        prompt("PLEASE INPUT A NUMBER, NO LETTERS:");
+    }
 
+    else {
+        let modulo = (numberConv % 2);
 
+        if (modulo == 1) {
+            document.getElementById('answer').style.display = "block";
+            document.getElementById('answer').innerHTML = "Your number is Uneven";
+        }
+
+        else {
+            document.getElementById('answer').style.display = "block";
+            document.getElementById('answer').innerHTML = "Your number is even";
+        }
+    }
 }
+oddEven();

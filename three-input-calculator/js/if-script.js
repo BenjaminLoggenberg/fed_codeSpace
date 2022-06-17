a7e36dab0c269f292943006fa54319f83996c21e
BenjaminLoggenberg <bloggenberg97@gmail.com>
2022-06-17 17:35:14 +0200

    three input calculator work

---
+++ three-input-calculator/js/if-script.js
@@ -0,0 +1,10 @@
+let number1 = prompt("Please enter a number:");
+let number2 = prompt("Please enter a second number:");
+let operator = prompt("Please enter an arithmetic operator:");
+
+function calculation() {
+    const answer = str.concat(number1 + ${ operator } + number2);
+
+}
+
+console.log(calculation);

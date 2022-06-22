4a65e52f118d0d777ac2aaab196bf2b83749d668
BenjaminLoggenberg <bloggenberg97@gmail.com>
2022-06-22 16:41:58 +0200

    calculator

--- calculator/calculatorScript.js
+++ calculator/calculatorScript.js
@@ -5,9 +5,11 @@ let expression = document.getElementById("display").value;
 
 function buildExpression(value) {
     expression = expression + value;
-    document.getElementById("display").value = expression;
+    document.getElementById("display").value = expression; // why do I need this line?
 }
 
 function calculate() {
     document.getElementById("display").value = eval(expression);
 }
+
+console.log(100)

c9bdfd5d47c7ae92b005a4a4603946773af39d5d
BenjaminLoggenberg <bloggenberg97@gmail.com>
2022-06-23 10:42:18 +0200

    userInput Linebreak
    added linebreak into project prompt \n

---
+++ userListChallenge/js/userLists.js
@@ -0,0 +1,4 @@
+let userDatabase = ['Pietie', 'Sannie', 'Benjamin', 'Chere', 'Koos', 'Daniel', 'Wentzel', 'Marco', 'Olivia', 'Luna'];
+
+
+let userInput = prompt("=======This is the Menu=======\n What would you like to do? \n 1) Add three users\n 2) Remove User \n 3) Display all Users \n 4) Sort Users");
